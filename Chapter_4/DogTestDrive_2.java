6cc1e5b9052e87cabddfd9a821bdbffc0c5320a3
John Shao <wjshao2010@gmail.com>
2020-07-30 01:01:00 -0400

    updated chapter 4 code

---
+++ Chapter_4/DogTestDrive_2.java
@@ -0,0 +1,18 @@
+class Dog_2 {
+    int size;
+    void bark (int numOfBarks) {
+        while(numOfBarks>0)
+        {
+            System.out.println("puff!");
+            numOfBarks--;
+        }
+    }
+}
+
+class DogTestDrive_2 {
+    public static void main(String[] args){
+        Dog_2 one = new Dog_2();
+        one.bark(3);
+    }
+
+}
---
+++ Chapter_4/gooddog.java
@@ -0,0 +1,33 @@
+class GoodDog {
+    private int size;
+    public int getSize() {
+        return size;
+    }
+    public void setSize(int s) { //only setter method can access the private instance variable
+        size = s;
+    }
+    void bark() {
+        if(size>80) {
+            System.out.println("Wooff!");
+        }
+        else if (size >14) {
+            System.out.println("puff!");
+        }
+        else {
+            System.out.println("YipYIP!");
+        }
+    }
+}
+
+class GoodDogTestDrive {
+    public static void main(String[] args) {
+        GoodDog one = new GoodDog();
+        one.setSize(70);
+        GoodDog two = new GoodDog();
+        two.setSize(80);
+        System.out.println("Dog one " + one.getSize());
+        System.out.println("Dog two " + two.getSize());
+        one.bark();
+        two.bark();
+    }
+}
